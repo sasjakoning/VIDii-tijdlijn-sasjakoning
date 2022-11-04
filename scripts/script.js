18364c96fa9ab20cecb98997cb0c3b066b7417d6
Sasja Koning <sasjakoning@hotmail.com>
2022-11-04 15:40:08 +0100

    added: elements and event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
-const painting1 = document.querySelector("main > ul > li:nth-of-type(1) > figure:nth-of-type(1)");
-const painting2 = document.querySelector("main > ul > li:nth-of-type(1) > figure:nth-of-type(2)");
+const mainSelector = "main > ul > li:nth-of-type";
+
+const painting1 = document.querySelector(mainSelector + "(1) > figure:nth-of-type(1)");
+const painting2 = document.querySelector((mainSelector + "(1) > figure:nth-of-type(2)"));
+const closet = document.querySelector((mainSelector + "(2) > figure:nth-of-type(1)"));
+const bookshelf = document.querySelector((mainSelector + "(2) > figure:nth-of-type(2)"));
+const bowlingBall = document.querySelector((mainSelector + "(2) > div"));
+
 
 const tom = document.querySelector("main > section:first-of-type > div:first-of-type")
 const jerry = document.querySelector("main > section:first-of-type > div:last-of-type")
-
-// let style = getComputedStyle(document.body)
-
-// console.log(style.getPropertyValue("--x-axis"))
-// console.log(style.getPropertyValue("--y-axis"))
 
 
 let painting1Counter = 0;
@@ -32,8 +33,39 @@ painting2.addEventListener("click", () => {
         painting2Counter++
     }else if (painting2Counter >= 1 && jerry.classList.contains("jerry-1")) {
         console.log("Move character")
+        painting2.classList.add("painting2-fall")
         jerry.classList.add("jerry-2")
         painting2Counter = 0;
 
+    }
+})
+
+let closetCounter = 0;
+
+console.log(closet.childNodes[3].childNodes)
+
+closet.addEventListener("click", () => {
+    console.log("clicked")
+    if(closetCounter <= 0 && jerry.classList.contains("jerry-2")){
+        closet.childNodes[3].childNodes[3].classList.add("closet-door")
+        closetCounter++
+    }else if (closetCounter >= 1 && jerry.classList.contains("jerry-2")) {
+        // jerry.classList.add("jerry-2")
+        closetCounter = 0;
+    }
+})
+
+
+let bookshelfCounter = 0;
+
+bookshelf.addEventListener("click", () => {
+    console.log("clicked")
+    if(bookshelfCounter <= 0 && jerry.classList.contains("jerry-2")){
+        bookshelfCounter++
+    }else if (bookshelfCounter >= 1 && jerry.classList.contains("jerry-2")) {
+        bookshelf.classList.add("bookshelf-fall")
+        bowlingBall.classList.add("bowlingball-fall")
+        // jerry.classList.add("jerry-2")
+        bookshelfCounter = 0;
     }
 })
